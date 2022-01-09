ac3f19708d78f54a2676a6034c1efafaa05df235
juanmatola <tolajuanmanuel@yahoo.com.ar>
2022-01-09 14:57:31 -0300

    main refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 import { mainAnimation } from './modules/mainAnimation.js';
 import { AnimatedNavbar } from './modules/AnimatedNavbar.js';
 
+mainAnimation();
+
 window.onscroll = mainAnimation;
 
 const nav = new AnimatedNavbar('myNav', 'openNav', '.menu__link');
